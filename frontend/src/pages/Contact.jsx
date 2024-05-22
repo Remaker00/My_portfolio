bb26efda5230916397c4153c2cefafa6ca20352f
Nishant Sourav <nishant.sharma8507966@gmail.com>
2024-05-22 13:19:01 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
     <div className='contact'>
       <h1>Get In Touch</h1>
       <p className="first">
-        I'm currently looking for full-time React Developer/Frontend Developer opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at, <a href="mailto:nishant.sharma8507966@gmail.com">nishant.sharma8507966@gmail.com</a>.
+        I'm currently looking for full-time Full-Stack Developer opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at, <a href="mailto:nishant.sharma8507966@gmail.com">nishant.sharma8507966@gmail.com</a>.
       </p>
       <p className="second">
         &lt;/&gt; with  by &#x2665; <span> Nishant Sourav </span> using <img src='next.svg' alt="Next Icon" />
